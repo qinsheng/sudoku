a2df79e1affa5b1b4598a26ff75b6bc867329703
Qinsheng <pacific169@qq.com>
2018-08-04 18:51:49 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,22 +4,24 @@ function Point() {
 }
 
 Point.prototype.set = function(value) {
+  if (this.ok()) {
+    return false;
+  }
   this.suspects = [ value ];
+  return true;
 }
 
 Point.prototype.exclude = function(value) {
   if (this.ok()) {
     return false;
   }
-
   for (let x = 0; x < this.suspects.length; x++) {
     if (this.suspects[x] === value) {
       this.suspects.splice(x, 1);
       return this.ok();
     }
   }
-
-  return false;
+   return false;
 }
 
 Point.prototype.ok = function() {
@@ -35,8 +37,7 @@ function Sudoku(mat) {
     for (let y = 0; y < 9; y++) {
       let point = new Point();
       if (mat[x][y] > 0) {
-        point.set(mat[x][y]);
-        this.count++;
+        this.count += point.set(mat[x][y]) ? 1 : 0;
       }
       line.push(point);
     }
@@ -63,23 +64,18 @@ Sudoku.prototype.refresh0 = function() {
       for (let y = 0; y < 9; y++) {
         let p = this.points[x][y];
         if (p.ok() && !p.refresh0) {
+          // exclude same line
           let v = p.suspects[0];
           for (let k = 0; k < 9; k++) {
-          	if (this.points[k][y].exclude(v)) {
-              this.count++;
-          	}
-          	if (this.points[x][k].exclude(v)) {
-              this.count++;
-          	}
+            this.count += this.points[k][y].exclude(v) ? 1 : 0;
+            this.count += this.points[x][k].exclude(v) ? 1 : 0;
           }
-
+          // exclude same palace
           let px = Math.floor(x / 3) * 3;
           let py = Math.floor(y / 3) * 3;
           for (let i = 0; i < 2; i++) {
             for (let j = 0; j < 2; j++) {
-          	  if (this.points[px + i][py + j].exclude(v)) {
-                this.count++;
-          	  }
+              this.count += this.points[px + i][py + j].exclude(v) ? 1 : 0;
             }
           }
         }
@@ -89,6 +85,7 @@ Sudoku.prototype.refresh0 = function() {
       break;
     }
   }
+  return this.count === 81;
 }
 
 const sudoku = new Sudoku([
